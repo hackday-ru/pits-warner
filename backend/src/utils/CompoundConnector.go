c94f4516c6c02187577164c0bb3790c35dc6ab47
soultoxik <cuerta.espada@gmail.com>
2016-04-16 18:35:54 +0300

    Ридеры вернулись

--- backend/src/utils/CompoundConnector.go
+++ backend/src/utils/CompoundConnector.go
@@ -10,3 +10,15 @@ type CompoundConnector struct  {
   cassConnector gocql.ClusterConfig
 }
 
+func (connector *CompoundConnector) init(redisAddr, cassAddr string)  {
+  connector.cassConnector = gocql.NewCluster(cassAddr)
+  connector.cassConnector.Keyspace = "geodb"
+
+  connector.redisConnector, _ = redis.NewClient(&redis.Options{
+    Addr:     redisAddr,
+    Password: "", // no password set
+    DB:       0,  // use default DB
+  })
+
+
+}
