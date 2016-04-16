4955ec117321a106a95ad93d45a4b53c6472cf73
Alexey Velikiy <gmpota@gmail.com>
2016-04-16 15:08:04 +0300

    begin server

--- backend/helloe.go
+++ backend/helloe.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Println("Hello, world!");
+  http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
